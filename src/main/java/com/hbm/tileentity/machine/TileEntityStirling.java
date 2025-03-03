84ca80e8c24f63ebe44248344e98f3b3e7915832
mn58835 <mn58835@tutanota.com>
2025-03-03 12:15:29 +0100

    engines

---
+++ src/main/java/com/hbm/tileentity/machine/TileEntityStirling.java
@@ -0,0 +1,259 @@
+package com.hbm.tileentity.machine;
+
+import java.io.IOException;
+
+import com.google.gson.JsonObject;
+import com.google.gson.stream.JsonWriter;
+import com.hbm.blocks.BlockDummyable;
+import com.hbm.blocks.ModBlocks;
+import com.hbm.lib.DirPos;
+import com.hbm.lib.ForgeDirection;
+import com.hbm.lib.HBMSoundHandler;
+import com.hbm.lib.Library;
+import com.hbm.tileentity.IConfigurableMachine;
+import com.hbm.tileentity.INBTPacketReceiver;
+import com.hbm.tileentity.TileEntityLoadedBase;
+
+import api.hbm.energymk2.IEnergyProviderMK2;
+import api.hbm.tile.IHeatSource;
+
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.ITickable;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+
+public class TileEntityStirling extends TileEntityLoadedBase implements INBTPacketReceiver, IEnergyProviderMK2, IConfigurableMachine, ITickable {
+
+    public long powerBuffer;
+    public int heat;
+    private int warnCooldown = 0;
+    private int overspeed = 0;
+    public boolean hasCog = true;
+
+
+    public float spin;
+    public float lastSpin;
+    private float xCoord = this.pos.getX();
+    private float yCoord = this.pos.getY();
+    private float zCoord = this.pos.getZ();
+
+    /* CONFIGURABLE CONSTANTS */
+    public static double diffusion = 0.1D;
+    public static double efficiency = 0.5D;
+    public static int maxHeatNormal = 300;
+    public static int maxHeatSteel = 1500;
+    public static int overspeedLimit = 300;
+
+    @Override
+    public void update() {
+
+        if(!world.isRemote) {
+
+            if(hasCog) {
+                this.powerBuffer = 0;
+                tryPullHeat();
+
+                this.powerBuffer = (long) (this.heat * (this.isCreative() ? 1 : this.efficiency));
+
+                if(warnCooldown > 0)
+                    warnCooldown--;
+
+                if(heat > maxHeat() && !isCreative()) {
+
+                    this.overspeed++;
+
+                    if(overspeed > 60 && warnCooldown == 0) {
+                        warnCooldown = 100;
+                        world.playSound(null, this.pos, HBMSoundHandler.warnOverspeed, SoundCategory.BLOCKS, 2F, 1F);
+                    }
+
+                    if(overspeed > overspeedLimit) {
+                        this.hasCog = false;
+                        this.world.newExplosion(null, xCoord + 0.5, yCoord + 1, zCoord + 0.5, 5F, false, false);
+
+                        int orientation = this.getBlockMetadata() - BlockDummyable.offset;
+                        ForgeDirection dir = ForgeDirection.getOrientation(orientation);
+                        //EntityCog cog = new EntityCog(world, xCoord + 0.5 + dir.offsetX, yCoord + 1, zCoord + 0.5 + dir.offsetZ).setOrientation(orientation).setMeta(this.getGeatMeta());
+                        ForgeDirection rot = dir.getRotation(ForgeDirection.DOWN);
+
+                        //TODO:COG
+//                        cog.motionX = rot.offsetX;
+//                        cog.motionY = 1 + (heat - maxHeat()) * 0.0001D;
+//                        cog.motionZ = rot.offsetZ;
+//                        world.spawnEntity(cog);
+
+                        this.markDirty();
+                    }
+
+                } else {
+                    this.overspeed = 0;
+                }
+            } else {
+                this.overspeed = 0;
+                this.warnCooldown = 0;
+            }
+
+            NBTTagCompound data = new NBTTagCompound();
+            data.setLong("power", powerBuffer);
+            data.setInteger("heat", heat);
+            data.setBoolean("hasCog", hasCog);
+            INBTPacketReceiver.networkPack(this, data, 150);
+
+            if(hasCog) {
+                for(DirPos pos : getConPos()) {
+                    this.tryProvide(world, pos.getPos().getX(), pos.getPos().getY(), pos.getPos().getZ(), pos.getDir());
+                }
+            } else {
+
+                if(this.powerBuffer > 0)
+                    this.powerBuffer--;
+            }
+
+            this.heat = 0;
+        } else {
+
+            float momentum = powerBuffer * 50F / ((float) maxHeat());
+
+            if(this.isCreative()) momentum = Math.min(momentum, 45F);
+
+            this.lastSpin = this.spin;
+            this.spin += momentum;
+
+            if(this.spin >= 360F) {
+                this.spin -= 360F;
+                this.lastSpin -= 360F;
+            }
+        }
+    }
+
+    public int getGeatMeta() {
+        return this.getBlockType() == ModBlocks.machine_stirling ? 0 : this.getBlockType() == ModBlocks.machine_stirling_creative ? 2 : 1;
+    }
+
+    public int maxHeat() {
+        return this.getBlockType() == ModBlocks.machine_stirling ? 300 : 1500;
+    }
+
+    public boolean isCreative() {
+        return this.getBlockType() == ModBlocks.machine_stirling_creative;
+    }
+
+    protected DirPos[] getConPos() {
+        return new DirPos[] {
+                new DirPos(xCoord + 2, yCoord, zCoord, Library.POS_X),
+                new DirPos(xCoord - 2, yCoord, zCoord, Library.NEG_X),
+                new DirPos(xCoord, yCoord, zCoord + 2, Library.POS_Z),
+                new DirPos(xCoord, yCoord, zCoord - 2, Library.NEG_Z)
+        };
+    }
+
+    @Override
+    public void networkUnpack(NBTTagCompound nbt) {
+        this.powerBuffer = nbt.getLong("power");
+        this.heat = nbt.getInteger("heat");
+        this.hasCog = nbt.getBoolean("hasCog");
+    }
+
+    protected void tryPullHeat() {
+        TileEntity con = world.getTileEntity(new BlockPos(xCoord, yCoord - 1, zCoord));
+
+        if(con instanceof IHeatSource) {
+            IHeatSource source = (IHeatSource) con;
+            int heatSrc = (int) (source.getHeatStored() * diffusion);
+
+            if(heatSrc > 0) {
+                source.useUpHeat(heatSrc);
+                this.heat += heatSrc;
+                return;
+            }
+        }
+
+        this.heat = Math.max(this.heat - Math.max(this.heat / 1000, 1), 0);
+    }
+
+    @Override
+    public void readFromNBT(NBTTagCompound nbt) {
+        super.readFromNBT(nbt);
+
+        this.powerBuffer = nbt.getLong("powerBuffer");
+        this.hasCog = nbt.getBoolean("hasCog");
+        this.overspeed = nbt.getInteger("overspeed");
+    }
+
+    @Override
+    public void writeToNBT(NBTTagCompound nbt) {
+        super.writeToNBT(nbt);
+
+        nbt.setLong("powerBuffer", powerBuffer);
+        nbt.setBoolean("hasCog", hasCog);
+        nbt.setInteger("overspeed", overspeed);
+    }
+
+    @Override
+    public void setPower(long power) {
+        this.powerBuffer = power;
+    }
+
+    @Override
+    public long getPower() {
+        return powerBuffer;
+    }
+
+    @Override
+    public long getMaxPower() {
+        return powerBuffer;
+    }
+
+    AxisAlignedBB bb = null;
+
+    @Override
+    public AxisAlignedBB getRenderBoundingBox() {
+
+        if(bb == null) {
+            bb = AxisAlignedBB.getBoundingBox(
+                    xCoord - 1,
+                    yCoord,
+                    zCoord - 1,
+                    xCoord + 2,
+                    yCoord + 2,
+                    zCoord + 2
+            );
+        }
+
+        return bb;
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public double getMaxRenderDistanceSquared() {
+        return 65536.0D;
+    }
+
+    @Override
+    public String getConfigName() {
+        return "stirling";
+    }
+
+    @Override
+    public void readIfPresent(JsonObject obj) {
+        diffusion = IConfigurableMachine.grab(obj, "D:diffusion", diffusion);
+        efficiency = IConfigurableMachine.grab(obj, "D:efficiency", efficiency);
+        maxHeatNormal = IConfigurableMachine.grab(obj, "I:maxHeatNormal", maxHeatNormal);
+        maxHeatSteel = IConfigurableMachine.grab(obj, "I:maxHeatSteel", maxHeatSteel);
+        overspeedLimit = IConfigurableMachine.grab(obj, "I:overspeedLimit", overspeedLimit);
+    }
+
+    @Override
+    public void writeConfig(JsonWriter writer) throws IOException {
+        writer.name("D:diffusion").value(diffusion);
+        writer.name("D:efficiency").value(efficiency);
+        writer.name("I:maxHeatNormal").value(maxHeatNormal);
+        writer.name("I:maxHeatSteel").value(maxHeatSteel);
+        writer.name("I:overspeedLimit").value(overspeedLimit);
+    }
+}
