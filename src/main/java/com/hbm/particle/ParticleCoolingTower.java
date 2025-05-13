a39f8028597a7c0f6350947f48fb834aede624df
MrNorwood <[email protected]>
2025-05-13 16:32:38 +0200

    forgot

--- src/main/java/com/hbm/particle/ParticleCoolingTower.java
+++ src/main/java/com/hbm/particle/ParticleCoolingTower.java
@@ -2,126 +2,119 @@ package com.hbm.particle;
 
 import com.hbm.lib.RefStrings;
 import com.hbm.main.ModEventHandlerClient;
-import com.hbm.render.NTMRenderHelper;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.particle.Particle;
 import net.minecraft.client.renderer.BufferBuilder;
-import net.minecraft.client.renderer.GlStateManager;
-import net.minecraft.client.renderer.RenderHelper;
-import net.minecraft.client.renderer.Tessellator;
-import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
-import org.lwjgl.opengl.GL11;
 
 public class ParticleCoolingTower extends Particle {
 
-	private static final ResourceLocation texture = new ResourceLocation(RefStrings.MODID + ":textures/particle/particle_base.png");
-	private float baseScale = 1.0F;
-	private float maxScale = 1.0F;
-	private float lift = 0.3F;
-	private float strafe = 0.075F;
-	private boolean windDir = true;
-	private float alphaMod = 0.25F;
+    private static final ResourceLocation texture = new ResourceLocation(RefStrings.MODID + ":textures/particle/particle_base.png");
+    private float baseScale = 1.0F;
+    private float maxScale = 1.0F;
+    private float lift = 0.3F;
+    private float strafe = 0.075F;
+    private boolean windDir = true;
+    private float alphaMod = 0.25F;
 
-	public ParticleCoolingTower( World world, double x, double y, double z) {
-		super(world, x, y, z);
-		this.particleAlpha = 0.30F; //Norwood: best solution to make the particle transparent with current minecraft version
-		this.particleRed = this.particleGreen = this.particleBlue = 0.9F + world.rand.nextFloat() * 0.05F;
-		this.canCollide = false;
-		this.setParticleTexture(ModEventHandlerClient.particle_base);
-	}
+    public ParticleCoolingTower(World world, double x, double y, double z) {
+        super(world, x, y, z);
+        this.particleAlpha = 0.30F; //Norwood: best solution to make the particle transparent with current minecraft version
+        this.particleRed = this.particleGreen = this.particleBlue = 0.9F + world.rand.nextFloat() * 0.05F;
+        this.canCollide = false;
+        this.setParticleTexture(ModEventHandlerClient.particle_base);
+    }
 
-	public void setBaseScale(float f) { this.baseScale = f; }
-	public void setMaxScale(float f) { this.maxScale = f; }
-	public void setLift(float f) { this.lift = f; }
-	public void setLife(int i) { this.particleMaxAge = i; }
-	public void setStrafe(float f) { this.strafe = f; }
-	public void noWind() { this.windDir = false; }
-	public void alphaMod(float mod) { this.alphaMod = mod; }
+    public void setBaseScale(float f) {
+        this.baseScale = f;
+    }
 
-	@Override
-	public void onUpdate() {
-		this.prevPosX = this.posX;
-		this.prevPosY = this.posY;
-		this.prevPosZ = this.posZ;
+    public void setMaxScale(float f) {
+        this.maxScale = f;
+    }
 
-		float ageScale = (float) this.particleAge / (float) this.particleMaxAge;
-		this.particleAlpha = alphaMod - ageScale * alphaMod;
-		this.particleScale = baseScale + (float) Math.pow((maxScale * ageScale - baseScale), 2);
+    public void setLift(float f) {
+        this.lift = f;
+    }
 
-		this.particleAge++;
-		if (lift > 0 && this.motionY < this.lift) this.motionY += 0.01F;
-		if (lift < 0 && this.motionY > this.lift) this.motionY -= 0.01F;
+    public void setLife(int i) {
+        this.particleMaxAge = i;
+    }
 
-		this.motionX += rand.nextGaussian() * strafe * ageScale;
-		this.motionZ += rand.nextGaussian() * strafe * ageScale;
+    public void setStrafe(float f) {
+        this.strafe = f;
+    }
 
-		if (windDir) {
-			this.motionX += 0.02 * ageScale;
-			this.motionZ -= 0.01 * ageScale;
-		}
+    public void noWind() {
+        this.windDir = false;
+    }
 
-		if (this.particleAge >= this.particleMaxAge) this.setExpired();
-		this.move(this.motionX, this.motionY, this.motionZ);
-		motionX *= 0.925;
-		motionY *= 0.925;
-		motionZ *= 0.925;
-	}
+    public void alphaMod(float mod) {
+        this.alphaMod = mod;
+    }
 
-	@Override
-	public void renderParticle(BufferBuilder buffer, Entity entityIn, float partialTicks, float rotationX, float rotationZ, float rotationYZ, float rotationXY, float rotationXZ){
-		float f = (float)this.particleTextureIndexX / 16.0F;
-		float f1 = f + 0.0624375F;
-		float f2 = (float)this.particleTextureIndexY / 16.0F;
-		float f3 = f2 + 0.0624375F;
-		float f4 = this.particleScale;
+    @Override
+    public void onUpdate() {
+        this.prevPosX = this.posX;
+        this.prevPosY = this.posY;
+        this.prevPosZ = this.posZ;
 
-		if (this.particleTexture != null)
-		{
-			f = this.particleTexture.getMinU();
-			f1 = this.particleTexture.getMaxU();
-			f2 = this.particleTexture.getMinV();
-			f3 = this.particleTexture.getMaxV();
-		}
+        float ageScale = (float) this.particleAge / (float) this.particleMaxAge;
+        this.particleAlpha = alphaMod - ageScale * alphaMod;
+        this.particleScale = baseScale + (float) Math.pow((maxScale * ageScale - baseScale), 2);
 
-		float f5 = (float)(this.prevPosX + (this.posX - this.prevPosX) * (double)partialTicks - interpPosX);
-		float f6 = (float)(this.prevPosY + (this.posY - this.prevPosY) * (double)partialTicks - interpPosY);
-		float f7 = (float)(this.prevPosZ + (this.posZ - this.prevPosZ) * (double)partialTicks - interpPosZ);
-		int i = this.getBrightnessForRender(partialTicks);
-		int j = i >> 16 & 65535;
-		int k = i & 65535;
-		Vec3d[] avec3d = new Vec3d[] {new Vec3d((double)(-rotationX * f4 - rotationXY * f4), (double)(-rotationZ * f4), (double)(-rotationYZ * f4 - rotationXZ * f4)), new Vec3d((double)(-rotationX * f4 + rotationXY * f4), (double)(rotationZ * f4), (double)(-rotationYZ * f4 + rotationXZ * f4)), new Vec3d((double)(rotationX * f4 + rotationXY * f4), (double)(rotationZ * f4), (double)(rotationYZ * f4 + rotationXZ * f4)), new Vec3d((double)(rotationX * f4 - rotationXY * f4), (double)(-rotationZ * f4), (double)(rotationYZ * f4 - rotationXZ * f4))};
+        this.particleAge++;
+        if (lift > 0 && this.motionY < this.lift) this.motionY += 0.01F;
+        if (lift < 0 && this.motionY > this.lift) this.motionY -= 0.01F;
 
-		if (this.particleAngle != 0.0F)
-		{
-			float f8 = this.particleAngle + (this.particleAngle - this.prevParticleAngle) * partialTicks;
-			float f9 = MathHelper.cos(f8 * 0.5F);
-			float f10 = MathHelper.sin(f8 * 0.5F) * (float)cameraViewDir.x;
-			float f11 = MathHelper.sin(f8 * 0.5F) * (float)cameraViewDir.y;
-			float f12 = MathHelper.sin(f8 * 0.5F) * (float)cameraViewDir.z;
-			Vec3d vec3d = new Vec3d((double)f10, (double)f11, (double)f12);
+        this.motionX += rand.nextGaussian() * strafe * ageScale;
+        this.motionZ += rand.nextGaussian() * strafe * ageScale;
 
-			for (int l = 0; l < 4; ++l)
-			{
-				avec3d[l] = vec3d.scale(2.0D * avec3d[l].dotProduct(vec3d)).add(avec3d[l].scale((double)(f9 * f9) - vec3d.dotProduct(vec3d))).add(vec3d.crossProduct(avec3d[l]).scale((double)(2.0F * f9)));
-			}
-		}
+        if (windDir) {
+            this.motionX += 0.02 * ageScale;
+            this.motionZ -= 0.01 * ageScale;
+        }
 
-		buffer.pos((double)f5 + avec3d[0].x, (double)f6 + avec3d[0].y, (double)f7 + avec3d[0].z).tex((double)f1, (double)f3).color(this.particleRed, this.particleGreen, this.particleBlue, this.particleAlpha).lightmap(j, k).endVertex();
-		buffer.pos((double)f5 + avec3d[1].x, (double)f6 + avec3d[1].y, (double)f7 + avec3d[1].z).tex((double)f1, (double)f2).color(this.particleRed, this.particleGreen, this.particleBlue, this.particleAlpha).lightmap(j, k).endVertex();
-		buffer.pos((double)f5 + avec3d[2].x, (double)f6 + avec3d[2].y, (double)f7 + avec3d[2].z).tex((double)f, (double)f2).color(this.particleRed, this.particleGreen, this.particleBlue, this.particleAlpha).lightmap(j, k).endVertex();
-		buffer.pos((double)f5 + avec3d[3].x, (double)f6 + avec3d[3].y, (double)f7 + avec3d[3].z).tex((double)f, (double)f3).color(this.particleRed, this.particleGreen, this.particleBlue, this.particleAlpha).lightmap(j, k).endVertex();
+        if (this.particleAge >= this.particleMaxAge) this.setExpired();
+        this.move(this.motionX, this.motionY, this.motionZ);
+        motionX *= 0.925;
+        motionY *= 0.925;
+        motionZ *= 0.925;
+    }
 
+    @Override
+    public void renderParticle(BufferBuilder buffer, Entity entityIn, float partialTicks, float rotationX, float rotationZ, float rotationYZ, float rotationXY, float rotationXZ) {
+        float f = (float) this.particleTextureIndexX / 16.0F;
+        float f1 = f + 0.0624375F;
+        float f2 = (float) this.particleTextureIndexY / 16.0F;
+        float f3 = f2 + 0.0624375F;
+        float f4 = this.particleScale;
 
-	}
+        if (this.particleTexture != null) {
+            f = this.particleTexture.getMinU();
+            f1 = this.particleTexture.getMaxU();
+            f2 = this.particleTexture.getMinV();
+            f3 = this.particleTexture.getMaxV();
+        }
 
-	@Override
-	public int getFXLayer(){
-		return 1;
-	}
+        float f5 = (float) (this.prevPosX + (this.posX - this.prevPosX) * (double) partialTicks - interpPosX);
+        float f6 = (float) (this.prevPosY + (this.posY - this.prevPosY) * (double) partialTicks - interpPosY);
+        float f7 = (float) (this.prevPosZ + (this.posZ - this.prevPosZ) * (double) partialTicks - interpPosZ);
+        int i = this.getBrightnessForRender(partialTicks);
+        int j = i >> 16 & 65535;
+        int k = i & 65535;
+        Vec3d[] avec3d = new Vec3d[]{new Vec3d((double) (-rotationX * f4 - rotationXY * f4), (double) (-rotationZ * f4), (double) (-rotationYZ * f4 - rotationXZ * f4)), new Vec3d((double) (-rotationX * f4 + rotationXY * f4), (double) (rotationZ * f4), (double) (-rotationYZ * f4 + rotationXZ * f4)), new Vec3d((double) (rotationX * f4 + rotationXY * f4), (double) (rotationZ * f4), (double) (rotationYZ * f4 + rotationXZ * f4)), new Vec3d((double) (rotationX * f4 - rotationXY * f4), (double) (-rotationZ * f4), (double) (rotationYZ * f4 - rotationXZ * f4))};
+        buffer.pos((double) f5 + avec3d[0].x, (double) f6 + avec3d[0].y, (double) f7 + avec3d[0].z).tex((double) f1, (double) f3).color(this.particleRed, this.particleGreen, this.particleBlue, this.particleAlpha).lightmap(j, k).endVertex();
+        buffer.pos((double) f5 + avec3d[1].x, (double) f6 + avec3d[1].y, (double) f7 + avec3d[1].z).tex((double) f1, (double) f2).color(this.particleRed, this.particleGreen, this.particleBlue, this.particleAlpha).lightmap(j, k).endVertex();
+        buffer.pos((double) f5 + avec3d[2].x, (double) f6 + avec3d[2].y, (double) f7 + avec3d[2].z).tex((double) f, (double) f2).color(this.particleRed, this.particleGreen, this.particleBlue, this.particleAlpha).lightmap(j, k).endVertex();
+        buffer.pos((double) f5 + avec3d[3].x, (double) f6 + avec3d[3].y, (double) f7 + avec3d[3].z).tex((double) f, (double) f3).color(this.particleRed, this.particleGreen, this.particleBlue, this.particleAlpha).lightmap(j, k).endVertex();
+    }
+
+    @Override
+    public int getFXLayer() {
+        return 1;
+    }
 
 }
