715151cb9be42547d4f6965f2d79637a7fba4b93
Vladimir <68227685+NoPressF@users.noreply.github.com>
2025-06-17 18:41:14 +0200

    fix: drawable position recipe items in GasCentrifugeRecipeHandler

--- src/main/java/com/hbm/handler/jei/GasCentrifugeRecipeHandler.java
+++ src/main/java/com/hbm/handler/jei/GasCentrifugeRecipeHandler.java
@@ -18,65 +18,66 @@ import net.minecraft.util.ResourceLocation;
 
 public class GasCentrifugeRecipeHandler implements IRecipeCategory<GasCentRecipe> {
 
-	public static final ResourceLocation gui_rl = new ResourceLocation(RefStrings.MODID, "textures/gui/jei/gui_jei_gas_centrifuge.png");
-	
-	protected final IDrawable background;
-	protected final IDrawableStatic powerStatic;
-	protected final IDrawableAnimated powerAnimated;
-	protected final IDrawableStatic progressStatic;
-	protected final IDrawableAnimated progressAnimated;
-	
-	public GasCentrifugeRecipeHandler(IGuiHelper help) {
-		background = help.createDrawable(gui_rl, 6, 15, 163, 55);
+  public static final ResourceLocation gui_rl =
+      new ResourceLocation(RefStrings.MODID, "textures/gui/jei/gui_jei_gas_centrifuge.png");
 
-		powerStatic = help.createDrawable(gui_rl, 176, 0, 16, 34);
-		powerAnimated = help.createAnimatedDrawable(powerStatic, 480, StartDirection.TOP, true);
-		
-		progressStatic = help.createDrawable(gui_rl, 192, 0, 22, 37);
-		progressAnimated = help.createAnimatedDrawable(progressStatic, 200, StartDirection.BOTTOM, false);
-	}
-	
-	@Override
-	public String getUid() {
-		return JEIConfig.GAS_CENT;
-	}
+  protected final IDrawable background;
+  protected final IDrawableStatic powerStatic;
+  protected final IDrawableAnimated powerAnimated;
+  protected final IDrawableStatic progressStatic;
+  protected final IDrawableAnimated progressAnimated;
 
-	@Override
-	public String getTitle() {
-		return I18nUtil.resolveKey("tile.machine_gascent.name");
-	}
+  public GasCentrifugeRecipeHandler(IGuiHelper help) {
+    background = help.createDrawable(gui_rl, 3, 6, 162, 54);
 
-	@Override
-	public String getModName() {
-		return RefStrings.MODID;
-	}
+    powerStatic = help.createDrawable(gui_rl, 168, 37, 16, 34);
+    powerAnimated = help.createAnimatedDrawable(powerStatic, 480, StartDirection.TOP, true);
 
-	@Override
-	public IDrawable getBackground() {
-		return background;
-	}
-	
-	@Override
-	public void drawExtras(Minecraft minecraft) {
-		powerAnimated.draw(minecraft, 2, 2);
-		progressAnimated.draw(minecraft, 89, 3);
-	}
+    progressStatic = help.createDrawable(gui_rl, 168, 0, 44, 37);
+    progressAnimated = help.createAnimatedDrawable(progressStatic, 150, StartDirection.LEFT, false);
+  }
 
-	@Override
-	public void setRecipe(IRecipeLayout recipeLayout, GasCentRecipe recipeWrapper, IIngredients ingredients) {
-		IGuiItemStackGroup guiItemStacks = recipeLayout.getItemStacks();
-		
-		guiItemStacks.init(0, true, 37, 19);
-		
-		guiItemStacks.init(1, false, 127, 1);
-		guiItemStacks.init(2, false, 145, 1);
-		guiItemStacks.init(3, false, 127, 37);
-		guiItemStacks.init(4, false, 145, 37);
-		
-		guiItemStacks.init(5, true, 1, 37);
-		
-		guiItemStacks.set(ingredients);
-		guiItemStacks.set(5, JeiRecipes.getBatteries());
-	}
+  @Override
+  public String getUid() {
+    return JEIConfig.GAS_CENT;
+  }
 
+  @Override
+  public String getTitle() {
+    return I18nUtil.resolveKey("tile.machine_gascent.name");
+  }
+
+  @Override
+  public String getModName() {
+    return RefStrings.MODID;
+  }
+
+  @Override
+  public IDrawable getBackground() {
+    return background;
+  }
+
+  @Override
+  public void drawExtras(Minecraft minecraft) {
+    powerAnimated.draw(minecraft, 1, 1);
+    progressAnimated.draw(minecraft, 72, 12);
+  }
+
+  @Override
+  public void setRecipe(
+      IRecipeLayout recipeLayout, GasCentRecipe recipeWrapper, IIngredients ingredients) {
+    IGuiItemStackGroup guiItemStacks = recipeLayout.getItemStacks();
+
+    guiItemStacks.init(0, true, 44, 18);
+
+    guiItemStacks.init(1, false, 126, 9);
+    guiItemStacks.init(2, false, 144, 9);
+    guiItemStacks.init(3, false, 126, 27);
+    guiItemStacks.init(4, false, 144, 27);
+
+    guiItemStacks.init(5, true, 0, 36);
+
+    guiItemStacks.set(ingredients);
+    guiItemStacks.set(5, JeiRecipes.getBatteries());
+  }
 }
