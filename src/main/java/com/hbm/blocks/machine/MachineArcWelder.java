66bd75da586cd97287438a6907b486d739c513c3
Vladimir <68227685+NoPressF@users.noreply.github.com>
2025-07-06 00:40:46 +0200

    cleanup: remove unused imports

--- src/main/java/com/hbm/blocks/machine/MachineArcWelder.java
+++ src/main/java/com/hbm/blocks/machine/MachineArcWelder.java
@@ -1,14 +1,12 @@
 package com.hbm.blocks.machine;
 
 import com.hbm.blocks.BlockDummyable;
-import com.hbm.lib.ForgeDirection;
 import com.hbm.tileentity.TileEntityProxyCombo;
 import com.hbm.tileentity.machine.TileEntityMachineArcWelder;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.EnumBlockRenderType;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
