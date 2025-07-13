ce1a15904a88816c3da5b5fb8191a7121e90c0bb
mlbv <51232730+mlbv@users.noreply.github.com>
2025-07-13 15:59:43 +0800

    feat: new tooltips for missiles

--- src/main/java/com/hbm/packet/PacketDispatcher.java
+++ src/main/java/com/hbm/packet/PacketDispatcher.java
@@ -42,8 +42,6 @@ public class PacketDispatcher {
 		wrapper.registerMessage(TEFluidTypePacketTest.Handler.class, TEFluidTypePacketTest.class, i++, Side.CLIENT);
 		//Packet for updating the bomber flying sound, I think
 		wrapper.registerMessage(LoopedEntitySoundPacket.Handler.class, LoopedEntitySoundPacket.class, i++, Side.CLIENT);
-		//Missile type for render sync with client
-		wrapper.registerMessage(TEMissilePacket.Handler.class, TEMissilePacket.class, i++, Side.CLIENT);
 		//Packet for sending designator data to server
 		wrapper.registerMessage(ItemDesignatorPacket.Handler.class, ItemDesignatorPacket.class, i++, Side.SERVER);
 		//New particle packet
