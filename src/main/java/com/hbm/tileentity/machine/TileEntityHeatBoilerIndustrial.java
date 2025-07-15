d61c8f5e3d5c6aaccb0482e2b63fc5e60cc55d4b
mlbv <51232730+mlbv@users.noreply.github.com>
2025-07-15 19:57:43 +0800

    fix: industrial boiler no sound

--- src/main/java/com/hbm/tileentity/machine/TileEntityHeatBoilerIndustrial.java
+++ src/main/java/com/hbm/tileentity/machine/TileEntityHeatBoilerIndustrial.java
@@ -9,7 +9,8 @@ import com.hbm.handler.threading.PacketThreading;
 import com.hbm.inventory.fluid.Fluids;
 import com.hbm.inventory.fluid.tank.FluidTankNTM;
 import com.hbm.inventory.fluid.trait.FT_Heatable;
-import com.hbm.inventory.fluid.trait.FT_Heatable.*;
+import com.hbm.inventory.fluid.trait.FT_Heatable.HeatingStep;
+import com.hbm.inventory.fluid.trait.FT_Heatable.HeatingType;
 import com.hbm.lib.DirPos;
 import com.hbm.lib.HBMSoundHandler;
 import com.hbm.lib.Library;
@@ -22,8 +23,6 @@ import com.hbm.tileentity.IConfigurableMachine;
 import com.hbm.tileentity.IFluidCopiable;
 import com.hbm.tileentity.TileEntityLoadedBase;
 import io.netty.buffer.ByteBuf;
-import java.io.IOException;
-import javax.annotation.Nullable;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
@@ -37,349 +36,300 @@ import net.minecraftforge.fml.common.network.NetworkRegistry;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-public class TileEntityHeatBoilerIndustrial extends TileEntityLoadedBase
-    implements IBufPacketReceiver,
-        ITickable,
-        IFluidStandardTransceiver,
-        IConfigurableMachine,
-        IFluidCopiable {
+import java.io.IOException;
 
-  public int heat;
-  public FluidTankNTM[] tanks;
-  public boolean isOn;
+public class TileEntityHeatBoilerIndustrial extends TileEntityLoadedBase implements IBufPacketReceiver, ITickable, IFluidStandardTransceiver,
+        IConfigurableMachine, IFluidCopiable {
 
-  private AudioWrapper audio;
-  private int audioTime;
+    /* CONFIGURABLE */
+    public static int maxHeat = 12_800_000;
+    public static double diffusion = 0.1D;
+    public int heat;
+    public FluidTankNTM[] tanks;
+    public boolean isOn;
+    AxisAlignedBB bb = null;
+    private AudioWrapper audio;
+    private int audioTime;
 
-  /* CONFIGURABLE */
-  public static int maxHeat = 12_800_000;
-  public static double diffusion = 0.1D;
+    public TileEntityHeatBoilerIndustrial() {
+        this.tanks = new FluidTankNTM[2];
 
-  public TileEntityHeatBoilerIndustrial() {
-    this.tanks = new FluidTankNTM[2];
+        this.tanks[0] = new FluidTankNTM(Fluids.WATER, 64_000);
+        this.tanks[1] = new FluidTankNTM(Fluids.STEAM, 64_000 * 100);
+    }
 
-    this.tanks[0] = new FluidTankNTM(Fluids.WATER, 64_000);
-    this.tanks[1] = new FluidTankNTM(Fluids.STEAM, 64_000 * 100);
-  }
+    @Override
+    public void update() {
 
-  @Override
-  public void update() {
+        if (!world.isRemote) {
 
-    if (!world.isRemote) {
-      
-      this.setupTanks();
-      this.updateConnections();
-      this.tryPullHeat();
+            this.setupTanks();
+            this.updateConnections();
+            this.tryPullHeat();
 
-      int light = this.world.getLightFor(EnumSkyBlock.SKY, pos);
-      if (light > 7 && TomSaveData.forWorld(world).fire > 1e-5) {
-        this.heat +=
-            (int)
-                ((maxHeat - heat)
-                    * 0.000005D); // constantly heat up 0.0005% of the remaining heat buffer for
-        // rampant but diminishing heating
-      }
-      
-      this.isOn = false;
-      this.tryConvert();
+            int light = this.world.getLightFor(EnumSkyBlock.SKY, pos);
+            if (light > 7 && TomSaveData.forWorld(world).fire > 1e-5) {
+                this.heat += (int) ((maxHeat - heat) * 0.000005D); // constantly heat up 0.0005% of the remaining heat buffer for
+                // rampant but diminishing heating
+            }
 
-      if (this.tanks[1].getFill() > 0) {
-        this.sendFluid();
-      }
-      
-      networkPackNT(25);
+            this.isOn = false;
+            this.tryConvert();
 
-    } else {
+            if (this.tanks[1].getFill() > 0) {
+                this.sendFluid();
+            }
 
-      if (this.isOn) audioTime = 20;
+            networkPackNT(25);
 
-      if (audioTime > 0) {
+        } else {
 
-        audioTime--;
+            if (this.isOn) audioTime = 20;
 
-        if (audio == null) {
-          audio = createAudioLoop();
-          audio.startSound();
-        } else if (!audio.isPlaying()) {
-          audio = rebootAudio(audio);
+            if (audioTime > 0) {
+
+                audioTime--;
+
+                if (audio == null) {
+                    audio = createAudioLoop();
+                    audio.startSound();
+                } else if (!audio.isPlaying()) {
+                    audio = rebootAudio(audio);
+                }
+
+                audio.updateVolume(getVolume(7F));
+                audio.keepAlive();
+
+            } else {
+
+                if (audio != null) {
+                    audio.stopSound();
+                    audio = null;
+                }
+            }
         }
+    }
 
-        audio.updateVolume(getVolume(1F));
-        audio.keepAlive();
+    @Override
+    public AudioWrapper createAudioLoop() {
+        return MainRegistry.proxy.getLoopedSound(HBMSoundHandler.boiler, SoundCategory.BLOCKS, pos.getX(), pos.getY(), pos.getZ(), 7F, 1.0F);
+    }
 
-      } else {
+    @Override
+    public void onChunkUnload() {
 
         if (audio != null) {
-          audio.stopSound();
-          audio = null;
+            audio.stopSound();
+            audio = null;
         }
-      }
-    }
-  }
-
-  @Override
-  public AudioWrapper createAudioLoop() {
-    return MainRegistry.proxy.getLoopedSound(
-        HBMSoundHandler.boiler,
-        SoundCategory.BLOCKS,
-        pos.getX(),
-        pos.getY(),
-        pos.getZ(),
-        0.125F,
-        1.0F);
-  }
-
-  @Override
-  public void onChunkUnload() {
-
-    if (audio != null) {
-      audio.stopSound();
-      audio = null;
-    }
-  }
-
-  @Override
-  public void invalidate() {
-
-    super.invalidate();
-
-    if (audio != null) {
-      audio.stopSound();
-      audio = null;
-    }
-  }
-
-  @Override
-  public void serialize(ByteBuf buf) {
-    super.serialize(buf);
-    buf.writeInt(heat);
-    tanks[0].serialize(buf);
-    tanks[1].serialize(buf);
-    buf.writeBoolean(this.isOn);
-  }
-
-  @Override
-  public void deserialize(ByteBuf buf) {
-    super.deserialize(buf);
-    this.heat = buf.readInt();
-    this.tanks[0].deserialize(buf);
-    this.tanks[1].deserialize(buf);
-    this.isOn = buf.readBoolean();
-  }
-
-  public void networkPackNT(int range) {
-    if (!world.isRemote)
-      PacketThreading.createAllAroundThreadedPacket(
-          new BufPacket(pos.getX(), pos.getY(), pos.getZ(), this),
-          new NetworkRegistry.TargetPoint(
-              this.world.provider.getDimension(), pos.getX(), pos.getY(), pos.getZ(), range));
-  }
-
-  protected void tryPullHeat() {
-    TileEntity con = world.getTileEntity(pos.down(1));
-
-    if (con instanceof IHeatSource source) {
-      int diff = source.getHeatStored() - this.heat;
-
-      if (diff == 0) {
-        return;
-      }
-
-      if (diff > 0) {
-        diff = (int) Math.ceil(diff * diffusion);
-        diff = Math.min(diff, maxHeat - this.heat);
-        source.useUpHeat(diff);
-        this.heat += diff;
-        if (this.heat > maxHeat) this.heat = maxHeat;
-        return;
-      }
     }
 
-    this.heat = Math.max(this.heat - Math.max(this.heat / 1000, 1), 0);
-  }
+    @Override
+    public void invalidate() {
 
-  protected void setupTanks() {
+        super.invalidate();
 
-    if (tanks[0].getTankType().hasTrait(FT_Heatable.class)) {
-      FT_Heatable trait = tanks[0].getTankType().getTrait(FT_Heatable.class);
-      if (trait.getEfficiency(HeatingType.BOILER) > 0) {
-        HeatingStep entry = trait.getFirstStep();
-        tanks[1].setTankType(entry.typeProduced);
-        tanks[1].changeTankSize(tanks[0].getMaxFill() * entry.amountProduced / entry.amountReq);
-        return;
-      }
+        if (audio != null) {
+            audio.stopSound();
+            audio = null;
+        }
     }
 
-    tanks[0].setTankType(Fluids.NONE);
-    tanks[1].setTankType(Fluids.NONE);
-  }
+    @Override
+    public void serialize(ByteBuf buf) {
+        super.serialize(buf);
+        buf.writeInt(heat);
+        tanks[0].serialize(buf);
+        tanks[1].serialize(buf);
+        buf.writeBoolean(this.isOn);
+    }
 
-  protected void tryConvert() {
+    @Override
+    public void deserialize(ByteBuf buf) {
+        super.deserialize(buf);
+        this.heat = buf.readInt();
+        this.tanks[0].deserialize(buf);
+        this.tanks[1].deserialize(buf);
+        this.isOn = buf.readBoolean();
+    }
 
-    if (tanks[0].getTankType().hasTrait(FT_Heatable.class)) {
-      FT_Heatable trait = tanks[0].getTankType().getTrait(FT_Heatable.class);
-      if (trait.getEfficiency(FT_Heatable.HeatingType.BOILER) > 0) {
+    public void networkPackNT(int range) {
+        if (!world.isRemote)
+            PacketThreading.createAllAroundThreadedPacket(new BufPacket(pos.getX(), pos.getY(), pos.getZ(), this),
+                    new NetworkRegistry.TargetPoint(this.world.provider.getDimension(), pos.getX(), pos.getY(), pos.getZ(), range));
+    }
 
-        HeatingStep entry = trait.getFirstStep();
-        int inputOps = this.tanks[0].getFill() / entry.amountReq;
-        int outputOps =
-            (this.tanks[1].getMaxFill() - this.tanks[1].getFill()) / entry.amountProduced;
-        int heatOps = this.heat / entry.heatReq;
+    protected void tryPullHeat() {
+        TileEntity con = world.getTileEntity(pos.down(1));
 
-        int ops = Math.min(inputOps, Math.min(outputOps, heatOps));
+        if (con instanceof IHeatSource source) {
+            int diff = source.getHeatStored() - this.heat;
 
-        this.tanks[0].setFill(this.tanks[0].getFill() - entry.amountReq * ops);
-        this.tanks[1].setFill(this.tanks[1].getFill() + entry.amountProduced * ops);
-        this.heat -= entry.heatReq * ops;
+            if (diff == 0) {
+                return;
+            }
 
-        if (ops > 0 && world.rand.nextInt(400) == 0) {
-          world.playSound(
-              null,
-              pos.getX() + 0.5,
-              pos.getY() + 2,
-              pos.getZ() + 0.5,
-              HBMSoundHandler.boilerGroanSounds[world.rand.nextInt(3)],
-              SoundCategory.BLOCKS,
-              0.5F,
-              1.0F);
+            if (diff > 0) {
+                diff = (int) Math.ceil(diff * diffusion);
+                diff = Math.min(diff, maxHeat - this.heat);
+                source.useUpHeat(diff);
+                this.heat += diff;
+                if (this.heat > maxHeat) this.heat = maxHeat;
+                return;
+            }
         }
 
-        if (ops > 0) {
-          this.isOn = true;
+        this.heat = Math.max(this.heat - Math.max(this.heat / 1000, 1), 0);
+    }
+
+    protected void setupTanks() {
+
+        if (tanks[0].getTankType().hasTrait(FT_Heatable.class)) {
+            FT_Heatable trait = tanks[0].getTankType().getTrait(FT_Heatable.class);
+            if (trait.getEfficiency(HeatingType.BOILER) > 0) {
+                HeatingStep entry = trait.getFirstStep();
+                tanks[1].setTankType(entry.typeProduced);
+                tanks[1].changeTankSize(tanks[0].getMaxFill() * entry.amountProduced / entry.amountReq);
+                return;
+            }
         }
-      }
-    }
-  }
 
-  private void updateConnections() {
-
-    for (DirPos dirPos : getConPos()) {
-      this.trySubscribe(
-          tanks[0].getTankType(),
-          world,
-          dirPos.getPos().getX(),
-          dirPos.getPos().getY(),
-          dirPos.getPos().getZ(),
-          dirPos.getDir());
-    }
-  }
-
-  private void sendFluid() {
-
-    for (DirPos dirPos : getConPos()) {
-      this.sendFluid(
-          tanks[1],
-          world,
-          dirPos.getPos().getX(),
-          dirPos.getPos().getY(),
-          dirPos.getPos().getZ(),
-          dirPos.getDir().getOpposite());
-    }
-  }
-
-  private DirPos[] getConPos() {
-    double x = pos.getX();
-    double y = pos.getY();
-    double z = pos.getZ();
-
-    return new DirPos[] {
-      new DirPos(x + 2, y, z, Library.POS_X),
-      new DirPos(x - 2, y, z, Library.NEG_X),
-      new DirPos(x, y, z + 2, Library.POS_Z),
-      new DirPos(x, y, z - 2, Library.NEG_Z),
-      new DirPos(x, y + 5, z, Library.POS_Y),
-    };
-  }
-
-  @Override
-  public void readFromNBT(NBTTagCompound nbt) {
-    super.readFromNBT(nbt);
-    tanks[0].readFromNBT(nbt, "water");
-    tanks[1].readFromNBT(nbt, "steam");
-    heat = nbt.getInteger("heat");
-  }
-
-  @Override
-  public @NotNull NBTTagCompound writeToNBT(NBTTagCompound nbt) {
-    super.writeToNBT(nbt);
-    tanks[0].writeToNBT(nbt, "water");
-    tanks[1].writeToNBT(nbt, "steam");
-    nbt.setInteger("heat", heat);
-    return nbt;
-  }
-
-  @Override
-  public FluidTankNTM[] getAllTanks() {
-    return tanks;
-  }
-
-  @Override
-  public FluidTankNTM[] getSendingTanks() {
-    return new FluidTankNTM[] {tanks[1]};
-  }
-
-  @Override
-  public FluidTankNTM[] getReceivingTanks() {
-    return new FluidTankNTM[] {tanks[0]};
-  }
-
-  AxisAlignedBB bb = null;
-
-  @Override
-  public @NotNull AxisAlignedBB getRenderBoundingBox() {
-
-    if (bb == null) {
-      bb =
-          new AxisAlignedBB(
-              pos.getX() - 1,
-              pos.getY(),
-              pos.getZ() - 1,
-              pos.getX() + 2,
-              pos.getY() + 5,
-              pos.getZ() + 2);
+        tanks[0].setTankType(Fluids.NONE);
+        tanks[1].setTankType(Fluids.NONE);
     }
 
-    return bb;
-  }
+    protected void tryConvert() {
 
-  @Override
-  @SideOnly(Side.CLIENT)
-  public double getMaxRenderDistanceSquared() {
-    return 65536.0D;
-  }
+        if (tanks[0].getTankType().hasTrait(FT_Heatable.class)) {
+            FT_Heatable trait = tanks[0].getTankType().getTrait(FT_Heatable.class);
+            if (trait.getEfficiency(FT_Heatable.HeatingType.BOILER) > 0) {
 
-  @Override
-  public String getConfigName() {
-    return "boilerIndustrial";
-  }
+                HeatingStep entry = trait.getFirstStep();
+                int inputOps = this.tanks[0].getFill() / entry.amountReq;
+                int outputOps = (this.tanks[1].getMaxFill() - this.tanks[1].getFill()) / entry.amountProduced;
+                int heatOps = this.heat / entry.heatReq;
 
-  @Override
-  public void readIfPresent(JsonObject obj) {
-    maxHeat = IConfigurableMachine.grab(obj, "I:maxHeat", maxHeat);
-    diffusion = IConfigurableMachine.grab(obj, "D:diffusion", diffusion);
-  }
+                int ops = Math.min(inputOps, Math.min(outputOps, heatOps));
 
-  @Override
-  public void writeConfig(JsonWriter writer) throws IOException {
-    writer.name("I:maxHeat").value(maxHeat);
-    writer.name("D:diffusion").value(diffusion);
-  }
+                this.tanks[0].setFill(this.tanks[0].getFill() - entry.amountReq * ops);
+                this.tanks[1].setFill(this.tanks[1].getFill() + entry.amountProduced * ops);
+                this.heat -= entry.heatReq * ops;
 
-  @Override
-  public boolean hasCapability(Capability<?> capability, @Nullable EnumFacing facing) {
-    if (capability == CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY) {
-      return true;
+                if (ops > 0 && world.rand.nextInt(400) == 0) {
+                    world.playSound(null, pos.getX() + 0.5, pos.getY() + 2, pos.getZ() + 0.5,
+                            HBMSoundHandler.boilerGroanSounds[world.rand.nextInt(3)], SoundCategory.BLOCKS, 10F, 1.0F);
+                }
+
+                if (ops > 0) {
+                    this.isOn = true;
+                }
+            }
+        }
     }
-    return super.hasCapability(capability, facing);
-  }
 
-  @Override
-  public <T> T getCapability(Capability<T> capability, @Nullable EnumFacing facing) {
-    if (capability == CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY) {
-      return CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY.cast(
-          new NTMFluidHandlerWrapper(this.getReceivingTanks(), null));
+    private void updateConnections() {
+
+        for (DirPos dirPos : getConPos()) {
+            this.trySubscribe(tanks[0].getTankType(), world, dirPos.getPos().getX(), dirPos.getPos().getY(), dirPos.getPos().getZ(), dirPos.getDir());
+        }
     }
-    return super.getCapability(capability, facing);
-  }
+
+    private void sendFluid() {
+
+        for (DirPos dirPos : getConPos()) {
+            this.sendFluid(tanks[1], world, dirPos.getPos().getX(), dirPos.getPos().getY(), dirPos.getPos().getZ(), dirPos.getDir().getOpposite());
+        }
+    }
+
+    private DirPos[] getConPos() {
+        double x = pos.getX();
+        double y = pos.getY();
+        double z = pos.getZ();
+
+        return new DirPos[]{new DirPos(x + 2, y, z, Library.POS_X), new DirPos(x - 2, y, z, Library.NEG_X), new DirPos(x, y, z + 2, Library.POS_Z),
+                new DirPos(x, y, z - 2, Library.NEG_Z), new DirPos(x, y + 5, z, Library.POS_Y),};
+    }
+
+    @Override
+    public void readFromNBT(NBTTagCompound nbt) {
+        super.readFromNBT(nbt);
+        tanks[0].readFromNBT(nbt, "water");
+        tanks[1].readFromNBT(nbt, "steam");
+        heat = nbt.getInteger("heat");
+    }
+
+    @Override
+    public @NotNull NBTTagCompound writeToNBT(NBTTagCompound nbt) {
+        super.writeToNBT(nbt);
+        tanks[0].writeToNBT(nbt, "water");
+        tanks[1].writeToNBT(nbt, "steam");
+        nbt.setInteger("heat", heat);
+        return nbt;
+    }
+
+    @Override
+    public FluidTankNTM[] getAllTanks() {
+        return tanks;
+    }
+
+    @Override
+    public FluidTankNTM[] getSendingTanks() {
+        return new FluidTankNTM[]{tanks[1]};
+    }
+
+    @Override
+    public FluidTankNTM[] getReceivingTanks() {
+        return new FluidTankNTM[]{tanks[0]};
+    }
+
+    @Override
+    public @NotNull AxisAlignedBB getRenderBoundingBox() {
+
+        if (bb == null) {
+            bb = new AxisAlignedBB(pos.getX() - 1, pos.getY(), pos.getZ() - 1, pos.getX() + 2, pos.getY() + 5, pos.getZ() + 2);
+        }
+
+        return bb;
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public double getMaxRenderDistanceSquared() {
+        return 65536.0D;
+    }
+
+    @Override
+    public String getConfigName() {
+        return "boilerIndustrial";
+    }
+
+    @Override
+    public void readIfPresent(JsonObject obj) {
+        maxHeat = IConfigurableMachine.grab(obj, "I:maxHeat", maxHeat);
+        diffusion = IConfigurableMachine.grab(obj, "D:diffusion", diffusion);
+    }
+
+    @Override
+    public void writeConfig(JsonWriter writer) throws IOException {
+        writer.name("I:maxHeat").value(maxHeat);
+        writer.name("D:diffusion").value(diffusion);
+    }
+
+    @Override
+    public boolean hasCapability(@NotNull Capability<?> capability, @Nullable EnumFacing facing) {
+        if (capability == CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY) {
+            return true;
+        }
+        return super.hasCapability(capability, facing);
+    }
+
+    @Override
+    public <T> T getCapability(@NotNull Capability<T> capability, @Nullable EnumFacing facing) {
+        if (capability == CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY) {
+            return CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY.cast(new NTMFluidHandlerWrapper(this.getReceivingTanks(), this.getSendingTanks()));
+        }
+        return super.getCapability(capability, facing);
+    }
 }
