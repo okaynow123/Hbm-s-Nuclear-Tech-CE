81c7b7fcdd12b67663ca814155a5f41088825729
mlbv <51232730+mlbv@users.noreply.github.com>
2025-07-24 18:46:12 +0800

    fix: template crate

--- src/main/java/com/hbm/inventory/container/ContainerCrateTemplate.java
+++ src/main/java/com/hbm/inventory/container/ContainerCrateTemplate.java
@@ -16,54 +16,44 @@ public class ContainerCrateTemplate extends Container {
     public ContainerCrateTemplate(InventoryPlayer invPlayer, TileEntityCrateTemplate tedf) {
         diFurnace = tedf;
 
-        for(int i = 0; i < 3; i++) {
-            for(int j = 0; j < 9; j++)
-            {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 9; j++) {
                 this.addSlotToContainer(new SlotItemHandler(tedf.inventory, j + i * 9, 8 + j * 18, 18 + i * 18));
             }
         }
 
-        for(int i = 0; i < 3; i++) {
-            for(int j = 0; j < 9; j++)
-            {
-                this.addSlotToContainer(new Slot(invPlayer, j + i * 9 + 9, 8 + j * 18, 84 + i * 18 + (18 * 3) + 2));
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 9; j++) {
+                this.addSlotToContainer(new Slot(invPlayer, j + i * 9 + 9, 8 + j * 18, 32 + i * 18 + (18 * 3)));
             }
         }
 
-        for(int i = 0; i < 9; i++) {
-            this.addSlotToContainer(new Slot(invPlayer, i, 8 + i * 18, 142 + (18 * 3) + 2));
+        for (int i = 0; i < 9; i++) {
+            this.addSlotToContainer(new Slot(invPlayer, i, 8 + i * 18, 90 + (18 * 3)));
         }
     }
 
     @NotNull
     @Override
-    public ItemStack transferStackInSlot(EntityPlayer p_82846_1_, int par2)
-    {
+    public ItemStack transferStackInSlot(EntityPlayer p_82846_1_, int par2) {
         ItemStack var3 = ItemStack.EMPTY;
-        Slot var4 = (Slot) this.inventorySlots.get(par2);
+        Slot var4 = this.inventorySlots.get(par2);
 
-        if (var4 != null && var4.getHasStack())
-        {
+        if (var4 != null && var4.getHasStack()) {
             ItemStack var5 = var4.getStack();
             var3 = var5.copy();
 
             if (par2 <= diFurnace.inventory.getSlots() - 1) {
-                if (!this.mergeItemStack(var5, diFurnace.inventory.getSlots(), this.inventorySlots.size(), true))
-                {
+                if (!this.mergeItemStack(var5, diFurnace.inventory.getSlots(), this.inventorySlots.size(), true)) {
                     return ItemStack.EMPTY;
                 }
-            }
-            else if (!this.mergeItemStack(var5, 0, diFurnace.inventory.getSlots(), false))
-            {
+            } else if (!this.mergeItemStack(var5, 0, diFurnace.inventory.getSlots(), false)) {
                 return ItemStack.EMPTY;
             }
 
-            if (var5.isEmpty())
-            {
+            if (var5.isEmpty()) {
                 var4.putStack(ItemStack.EMPTY);
-            }
-            else
-            {
+            } else {
                 var4.onSlotChanged();
             }
 
