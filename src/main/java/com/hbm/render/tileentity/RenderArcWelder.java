fdf292919bbd0a7d1914504ae750064563fbfa7d
mlbv <51232730+mlbv@users.noreply.github.com>
2025-07-25 22:20:15 +0800

    fix: welder and soldering item render

--- src/main/java/com/hbm/render/tileentity/RenderArcWelder.java
+++ src/main/java/com/hbm/render/tileentity/RenderArcWelder.java
@@ -13,35 +13,20 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
-public class RenderArcWelder extends TileEntitySpecialRenderer<TileEntityMachineArcWelder>
-        implements IItemRendererProvider {
+public class RenderArcWelder extends TileEntitySpecialRenderer<TileEntityMachineArcWelder> implements IItemRendererProvider {
     @Override
-    public void render(
-            @NotNull TileEntityMachineArcWelder arc_welder,
-            double x,
-            double y,
-            double z,
-            float partialTicks,
-            int destroyStage,
-            float alpha) {
+    public void render(@NotNull TileEntityMachineArcWelder arc_welder, double x, double y, double z, float partialTicks, int destroyStage,
+                       float alpha) {
         GlStateManager.pushMatrix();
         GlStateManager.translate(x + 0.5, y, z + 0.5);
         GlStateManager.enableLighting();
         GlStateManager.enableCull();
 
         switch (arc_welder.getBlockMetadata() - BlockDummyable.offset) {
-            case 2:
-                GlStateManager.rotate(90, 0F, 1F, 0F);
-                break;
-            case 4:
-                GlStateManager.rotate(180, 0F, 1F, 0F);
-                break;
-            case 3:
-                GlStateManager.rotate(270, 0F, 1F, 0F);
-                break;
-            case 5:
-                GlStateManager.rotate(0, 0F, 1F, 0F);
-                break;
+            case 2 -> GlStateManager.rotate(90, 0F, 1F, 0F);
+            case 4 -> GlStateManager.rotate(180, 0F, 1F, 0F);
+            case 3 -> GlStateManager.rotate(270, 0F, 1F, 0F);
+            case 5 -> GlStateManager.rotate(0, 0F, 1F, 0F);
         }
 
         GlStateManager.translate(-0.5, 0, 0);
@@ -51,16 +36,19 @@ public class RenderArcWelder extends TileEntitySpecialRenderer<TileEntityMachine
 
         if (!arc_welder.display.isEmpty()) {
             GlStateManager.pushMatrix();
-            GlStateManager.translate(0.0625D * 2.5D, 1.125D, 0D);
+            GlStateManager.translate(0D, 1.125D, 0D);
             GlStateManager.enableLighting();
             GlStateManager.rotate(90, 0F, 1F, 0F);
             GlStateManager.rotate(-90, 1F, 0F, 0F);
-            ItemStack stack = arc_welder.display.copy();
-            GlStateManager.scale(0.8, 0.8, 0.8);
-            GlStateManager.translate(0, +0.2, 0);
-            Minecraft.getMinecraft()
-                    .getRenderItem()
-                    .renderItem(stack, ItemCameraTransforms.TransformType.FIXED);
+
+            if (!arc_welder.display.isEmpty()) {
+                ItemStack stack = arc_welder.display.copy();
+                stack.setCount(1);
+                // 0.5128205 * 1.5 = 0.76923075
+                // see 1.7 net.minecraft.client.renderer.entity.RenderItem line 203
+                GlStateManager.scale(0.76923075F, 0.76923075F, 0.76923075F);
+                Minecraft.getMinecraft().getRenderItem().renderItem(stack, ItemCameraTransforms.TransformType.NONE);
+            }
             GlStateManager.popMatrix();
         }
 
--- src/main/java/com/hbm/render/tileentity/RenderSolderingStation.java
+++ src/main/java/com/hbm/render/tileentity/RenderSolderingStation.java
@@ -9,84 +9,65 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.block.model.ItemCameraTransforms;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
-import net.minecraft.entity.item.EntityItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
-public class RenderSolderingStation
-    extends TileEntitySpecialRenderer<TileEntityMachineSolderingStation>
-    implements IItemRendererProvider {
-  @Override
-  public void render(
-      @NotNull TileEntityMachineSolderingStation soldering_station,
-      double x,
-      double y,
-      double z,
-      float partialTicks,
-      int destroyStage,
-      float alpha) {
-    GlStateManager.pushMatrix();
-    GlStateManager.translate(x + 0.5, y, z + 0.5);
-    GlStateManager.enableLighting();
-    GlStateManager.enableCull();
+public class RenderSolderingStation extends TileEntitySpecialRenderer<TileEntityMachineSolderingStation> implements IItemRendererProvider {
+    @Override
+    public void render(@NotNull TileEntityMachineSolderingStation soldering_station, double x, double y, double z, float partialTicks,
+                       int destroyStage, float alpha) {
+        GlStateManager.pushMatrix();
+        GlStateManager.translate(x + 0.5, y, z + 0.5);
+        GlStateManager.enableLighting();
+        GlStateManager.enableCull();
 
-    switch (soldering_station.getBlockMetadata() - BlockDummyable.offset) {
-      case 2:
-        GlStateManager.rotate(90, 0F, 1F, 0F);
-        break;
-      case 4:
-        GlStateManager.rotate(180, 0F, 1F, 0F);
-        break;
-      case 3:
-        GlStateManager.rotate(270, 0F, 1F, 0F);
-        break;
-      case 5:
-        GlStateManager.rotate(0, 0F, 1F, 0F);
-        break;
-    }
+        switch (soldering_station.getBlockMetadata() - BlockDummyable.offset) {
+            case 2 -> GlStateManager.rotate(90, 0F, 1F, 0F);
+            case 4 -> GlStateManager.rotate(180, 0F, 1F, 0F);
+            case 3 -> GlStateManager.rotate(270, 0F, 1F, 0F);
+            case 5 -> GlStateManager.rotate(0, 0F, 1F, 0F);
+        }
 
-    GlStateManager.translate(-0.5, 0, 0.5);
+        GlStateManager.translate(-0.5, 0, 0.5);
 
-    bindTexture(ResourceManager.soldering_station_tex);
-    ResourceManager.soldering_station.renderAll();
-
-    if (!soldering_station.display.isEmpty() ) {
-      GlStateManager.pushMatrix();
-      GlStateManager.translate(0.0625D * 2.5D, 1.125D, 0D);
-      GlStateManager.enableLighting();
-      GlStateManager.rotate(90, 0F, 1F, 0F);
-      GlStateManager.rotate(-90, 1F, 0F, 0F);
-
-        ItemStack stack = soldering_station.display.copy();
-        GlStateManager.scale(0.8,0.8, 0.8);
-        GlStateManager.translate(-0.2, +0.2, 0);
-        Minecraft.getMinecraft()
-            .getRenderItem()
-            .renderItem(stack, ItemCameraTransforms.TransformType.FIXED);
-      GlStateManager.popMatrix();
-    }
-
-    GlStateManager.popMatrix();
-  }
-
-  @Override
-  public Item getItemForRenderer() {
-    return Item.getItemFromBlock(ModBlocks.machine_soldering_station);
-  }
-
-  @Override
-  public ItemRenderBase getRenderer(Item item) {
-    return new ItemRenderBase() {
-      public void renderInventory() {
-        GlStateManager.translate(0, -1, 0);
-        GlStateManager.scale(5, 5, 5);
-      }
-
-      public void renderCommon() {
         bindTexture(ResourceManager.soldering_station_tex);
         ResourceManager.soldering_station.renderAll();
-      }
-    };
-  }
+
+        if (!soldering_station.display.isEmpty()) {
+            GlStateManager.pushMatrix();
+            GlStateManager.translate(0D, 1.125D, 0D);
+            GlStateManager.enableLighting();
+            GlStateManager.rotate(90, 0F, 1F, 0F);
+            GlStateManager.rotate(-90, 1F, 0F, 0F);
+            ItemStack stack = soldering_station.display.copy();
+            stack.setCount(1);
+            // 0.5128205 * 1.5 = 0.76923075, net.minecraft.client.renderer.entity.RenderItem line 203
+            GlStateManager.scale(0.76923075F, 0.76923075F, 0.76923075F);
+            Minecraft.getMinecraft().getRenderItem().renderItem(stack, ItemCameraTransforms.TransformType.NONE);
+            GlStateManager.popMatrix();
+        }
+
+        GlStateManager.popMatrix();
+    }
+
+    @Override
+    public Item getItemForRenderer() {
+        return Item.getItemFromBlock(ModBlocks.machine_soldering_station);
+    }
+
+    @Override
+    public ItemRenderBase getRenderer(Item item) {
+        return new ItemRenderBase() {
+            public void renderInventory() {
+                GlStateManager.translate(0, -1, 0);
+                GlStateManager.scale(5, 5, 5);
+            }
+
+            public void renderCommon() {
+                bindTexture(ResourceManager.soldering_station_tex);
+                ResourceManager.soldering_station.renderAll();
+            }
+        };
+    }
 }
