6fe483f8227d37e4c6f1500e351bcc8ba1d44eeb
mlbv <51232730+mlbv@users.noreply.github.com>
2025-07-29 02:28:37 +0800

    chore: discard Spaghetti and Untested on compile

--- src/main/java/com/hbm/interfaces/Spaghetti.java
+++ src/main/java/com/hbm/interfaces/Spaghetti.java
@@ -1,8 +1,11 @@
 package com.hbm.interfaces;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 //Universal notation for shitty code
 //I know TODO is a thing, but it's nice to hover over a class and see wtf is wrong with it
+@Retention(RetentionPolicy.SOURCE)
 public @interface Spaghetti {
-
-	public String value();
+	String value();
 }
--- src/main/java/com/hbm/interfaces/Untested.java
+++ src/main/java/com/hbm/interfaces/Untested.java
@@ -1,5 +1,9 @@
 package com.hbm.interfaces;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.SOURCE)
 public @interface Untested {
     String value() default "";
 }
