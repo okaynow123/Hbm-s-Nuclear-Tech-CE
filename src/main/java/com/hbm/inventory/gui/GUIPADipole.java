1a6242e63961d271ad0a613c7c96d3d40980afbb
mlbv <51232730+mlbv@users.noreply.github.com>
2025-07-31 22:20:02 +0800

    fix: PA dipole gui crash
    resolves #324

--- src/main/java/com/hbm/inventory/gui/GUIPADipole.java
+++ src/main/java/com/hbm/inventory/gui/GUIPADipole.java
@@ -100,7 +100,7 @@ public class GUIPADipole extends GuiInfoContainer {
 
     @Override
     protected void drawGuiContainerForegroundLayer(int mouseX, int mouseY) {
-        String name = this.dipole.getDisplayName().getUnformattedText();
+        String name = this.dipole.hasCustomInventoryName() ? this.dipole.getInventoryName() : I18n.format(this.dipole.getInventoryName());
         this.fontRenderer.drawString(name, this.xSize / 2 - this.fontRenderer.getStringWidth(name) / 2 - 9, 6, 4210752);
         this.fontRenderer.drawString(I18n.format("container.inventory"), 8, this.ySize - 96 + 2, 4210752);
 
