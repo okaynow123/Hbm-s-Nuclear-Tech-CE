6ec308d651c5b9bd16a1c00584162e048a3e7356
mlbv <51232730+mlbv@users.noreply.github.com>
2025-08-01 22:08:59 +0800

    [ci skip] docs: update javadoc for AutoRegister

--- processor/src/main/java/com/hbm/interfaces/AutoRegister.java
+++ processor/src/main/java/com/hbm/interfaces/AutoRegister.java
@@ -3,63 +3,108 @@ package com.hbm.interfaces;
 import java.lang.annotation.*;
 
 /**
- * Annotation for registering entities, tile entities, and their renderers
- * <li> For entity renderer that extends Render<E>:<br>
- * entity:  the entity class to bind the renderer onto.<br>
- * factory: the class that contains the static final IRenderFactory field, which MUST be named "FACTORY".
- * <li> For itemStack renderer that inherits TEISRBase: <br>
- * item: the static field name of the item in the ModItems class (e.g., "hf_sword" for ModItems.hf_sword)
- * <li> For tileentity renderer that extends TileEntitySpecialRenderer<T>: <br>
- * tileentity: the tileentity class to bind the renderer onto
- * <li> For entity: <br>
- * name: custom name for the entity. must not be empty.
- * trackingRange: the tracking range for the entity.
- * updateFrequency: the update frequency for the entity.
- * sendVelocityUpdates: whether to send velocity updates for the entity.
- * <li> For tileentity: <br>
- * name: custom name for the tileentity. If empty, a name will be generated from the class name.
+ * A versatile annotation that automates the registration of nearly everything.
+ * <p>
+ * This annotation is processed at compile time to generate the necessary registration
+ * boilerplate code. It can be applied to entities, tile entities, and renderer classes.
+ * The behavior of the annotation changes based on the type of class it is applied to.
+ * <ul>
+ *   <li><b>Entity Renderer:</b> When annotating a class that extends {@code Render<E>},
+ *       use {@link #entity()} to specify the target entity class. If omitted, the processor will attempt
+ *       to infer the entity type from the renderer's generic parameter. If the factory method requires a specific
+ *       {@code IRenderFactory}, specify its name in {@link #factory()}.</li>
+ *
+ *   <li><b>Item Stack Renderer:</b> When annotating a class that extends
+ *       {@code TileEntityItemStackRenderer},
+ *       use {@link #item()} to specify the static field name of the {@code Item} this renderer should be bound to.</li>
+ *
+ *   <li><b>Tile Entity Renderer:</b> When annotating a class that extends {@code TileEntitySpecialRenderer<T>},
+ *       use {@link #tileentity()} to specify the target tile entity class. If omitted, the processor will
+ *       attempt to infer the tile entity type from the renderer's generic parameter.</li>
+ *
+ *   <li><b>Entity:</b> When annotating a class that extends {@code Entity},
+ *       the {@link #name()} field is mandatory. Use {@link #trackingRange()}, {@link #updateFrequency()},
+ *       and {@link #sendVelocityUpdates()} to configure its behavior.</li>
+ *
+ *   <li><b>Tile Entity:</b> When annotating a class that extends {@code TileEntity},
+ *       use {@link #name()} to provide a custom registration name. If the name is omitted, one will be
+ *       generated automatically from the class name (e.g., {@code TileEntityAwesome} becomes {@code "tileentity_awesome"}).</li>
+ * </ul>
+ *
+ * @see com.hbm.processor.AutoRegisterProcessor The annotation processor that handles this annotation.
  */
 @Retention(RetentionPolicy.SOURCE)
 @Target(ElementType.TYPE)
 @Repeatable(AutoRegisterContainer.class)
 public @interface AutoRegister {
+
     /**
-     * The entity class to bind the annotated renderer onto.
+     * Specifies the entity class that this renderer should be bound to. Optional.
+     * <p>
+     * This is applicable when the annotated class is an entity renderer
+     * (i.e., extends {@code Render<E>}).
+     * If this is not set, the processor will attempt to infer the entity type from the
+     * renderer's generic type parameter.
      */
     Class<?> entity() default Object.class;
 
     /**
-     * The field name of the Item to bind the TEISR onto (e.g., "my_item" for ModItems.my_item).
+     * Specifies the name of the static {@code Item} field to which this
+     * {@code TileEntityItemStackRenderer} should be bound.
+     * <p>
+     * For example, a value of {@code "hf_sword"} would target a field like {@code ModItems.hf_sword}.
+     * This is required when annotating a TEISR class.
      */
     String item() default "";
 
     /**
-     * The TileEntity class to bind the annotated renderer onto.
+     * Specifies the tile entity class that this special renderer should be bound to.
+     * Optional.
+     * <p>
+     * This is applicable when the annotated class is a tile entity renderer
+     * (i.e., extends {@code TileEntitySpecialRenderer}).
+     * If this is not set, the processor will attempt to infer the tile entity type from the
+     * renderer's generic type parameter.
      */
     Class<?> tileentity() default Object.class;
 
     /**
-     * The Class containing the static "FACTORY" instance for an entity renderer.
+     * Specifies the name of the static {@code IRenderFactory} field to be used for
+     * instantiating an entity renderer. Optional.
+     * <p>
+     * The field must be a {@code public static final} member of the annotated renderer class.
+     * If this is not set, the processor will use {@code AnnotatedClass::new}.
      */
     String factory() default "";
 
     /**
-     * Custom registration name for an entity or tile entity.
+     * The unique registration name for an entity or tile entity.
+     * <p>
+     * For entities, this field is <strong>mandatory</strong> and must not be empty.
+     * <p>
+     * For tile entities, this field is optional. If left empty, a name will be
+     * automatically generated from the tile entity's class name.
      */
     String name() default "";
 
     /**
-     * Entity tracking range.
+     * The range at which MC will send tracking updates. Optional.
+     * <p>
+     * Only applicable when annotating an entity class. Default = 250.
      */
     int trackingRange() default 250;
 
     /**
-     * Entity update frequency.
+     * The frequency of tracking updates. Optional.
+     * <p>
+     * Only applicable when annotating an entity class. Default = 1.
      */
     int updateFrequency() default 1;
 
     /**
-     * Whether the entity sends velocity updates.
+     * Whether to send velocity information packets. Optional.
+     * <p>
+     * Only applicable when annotating an entity class. Default = true.
      */
     boolean sendVelocityUpdates() default true;
 }
