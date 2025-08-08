58b42f861c1ef5ce95922e4aa3d1a4fbdc26c58b
MrNorwood <[email protected]>
2025-08-08 21:16:48 +0200

    revert few syntax changes

--- src/main/java/com/hbm/qmaw/QMAWLoader.java
+++ src/main/java/com/hbm/qmaw/QMAWLoader.java
@@ -1,11 +1,29 @@
 package com.hbm.qmaw;
 
-import com.google.gson.*;
+import java.io.File;
+import java.io.FileReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.sql.Ref;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map.Entry;
+import java.util.function.Predicate;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.hbm.interfaces.Spaghetti;
 import com.hbm.inventory.RecipesCommon.ComparableStack;
 import com.hbm.inventory.recipes.SerializableRecipe;
 import com.hbm.items.ModItems;
 import com.hbm.main.MainRegistry;
+
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.resources.*;
 import net.minecraft.item.ItemStack;
@@ -14,18 +32,7 @@ import net.minecraftforge.client.resource.ISelectiveResourceReloadListener;
 import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.ModContainer;
 import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
-
-import java.io.File;
-import java.io.FileReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.Enumeration;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map.Entry;
-import java.util.function.Predicate;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipFile;
+import net.minecraftforge.fml.relauncher.ReflectionHelper;
 
 @Spaghetti("everything here is a fucking mess")
 public class QMAWLoader implements ISelectiveResourceReloadListener {
@@ -37,17 +44,22 @@ public class QMAWLoader implements ISelectiveResourceReloadListener {
     public static HashMap<String, QuickManualAndWiki> qmaw = new HashMap<>();
     public static HashMap<ComparableStack, QuickManualAndWiki> triggers = new HashMap<>();
 
-    /**
-     * Searches the asset folder for QMAW format JSON files and adds entries based on that
-     */
+    @Override
+    public void onResourceManagerReload(IResourceManager resourceManager, Predicate<IResourceType> resourcePredicate) {
+        long timestamp = System.currentTimeMillis();
+        MainRegistry.logger.info("[QMAW] Reloading manual...");
+        init();
+        MainRegistry.logger.info("[QMAW] Loaded " + qmaw.size() + " manual entries! (" + (System.currentTimeMillis() - timestamp) + "ms)");
+    }
+
+    /** Searches the asset folder for QMAW format JSON files and adds entries based on that */
     public static void init() {
         //Force loads ALL packs. you have no say in this
         for (ModContainer mod : Loader.instance().getActiveModList()) {
             File src = mod.getSource();
             if (src.isFile()) {
                 modResourcePacks.add(new FileResourcePack(src));
-            }
-            if (src.isDirectory()) {
+            } if (src.isDirectory()) {
                 folderResourcePacks.add(new FolderResourcePack(src));
             }
         }
@@ -57,14 +69,13 @@ public class QMAWLoader implements ISelectiveResourceReloadListener {
         agonyEngine();
     }
 
-    /**
-     * "digital equivalent to holywater" yielded few results on google, if only i had the answer i would drown this entire class in it <br><br>
+    /** "digital equivalent to holywater" yielded few results on google, if only i had the answer i would drown this entire class in it <br><br>
      * This affront to god can load QMAW definition files from four different sources:<br>
      * * Any mod's jar that has registered itself to include QMAW files<br>
      * * The dev environment, because "the mod file" would in this case be this very class file, and that's incorrect<br>
      * * ZIP-based resource packs<br>
      * * Folder-based resource packs
-     */
+     * */
     public static void agonyEngine() {
 
         // I should stop accepting random PRs when in gym man...
@@ -142,20 +153,14 @@ public class QMAWLoader implements ISelectiveResourceReloadListener {
         }
     }
 
-    public static void logPackAttempt(String name) {
-        MainRegistry.logger.info("[QMAW] Dissecting resource " + name);
-    }
 
-    public static void logFoundManual(String name) {
-        MainRegistry.logger.info("[QMAW] Found manual " + name);
-    }
+    public static void logPackAttempt(String name) { MainRegistry.logger.info("[QMAW] Dissecting resource " + name); }
+    public static void logFoundManual(String name) { MainRegistry.logger.info("[QMAW] Found manual " + name); }
 
-    /**
-     * You put your white gloves on, you get your hand in there, and then you iterate OVER THE ENTIRE FUCKING ZIP until we find things we deem usable
-     */
+    /** You put your white gloves on, you get your hand in there, and then you iterate OVER THE ENTIRE FUCKING ZIP until we find things we deem usable */
     public static void dissectZip(File zipFile) {
 
-        if (zipFile == null) {
+        if(zipFile == null) {
             MainRegistry.logger.info("[QMAW] Pack file does not exist!");
             return;
         }
@@ -166,10 +171,10 @@ public class QMAWLoader implements ISelectiveResourceReloadListener {
             zip = new ZipFile(zipFile);
             Enumeration<? extends ZipEntry> enumerator = zip.entries();
 
-            while (enumerator.hasMoreElements()) {
+            while(enumerator.hasMoreElements()) {
                 ZipEntry entry = enumerator.nextElement();
                 String name = entry.getName();
-                if (name.startsWith("assets/hbm/manual/") && name.endsWith(".json")) {
+                if(name.startsWith("assets/hbm/manual/") && name.endsWith(".json")) {
                     InputStream fileStream = zip.getInputStream(entry);
                     InputStreamReader reader = new InputStreamReader(fileStream);
                     try {
@@ -178,95 +183,80 @@ public class QMAWLoader implements ISelectiveResourceReloadListener {
                         registerJson(manName, obj);
                         reader.close();
                         logFoundManual(manName);
-                    } catch (Exception ex) {
+                    } catch(Exception ex) {
                         MainRegistry.logger.info("[QMAW] Error reading manual " + name + ": " + ex);
                     }
                 }
             }
 
-        } catch (Exception ex) {
+        } catch(Exception ex) {
             MainRegistry.logger.info("[QMAW] Error dissecting zip " + zipFile.getName() + ": " + ex);
         } finally {
             try {
-                if (zip != null) zip.close();
-            } catch (Exception ex) {
-            }
+                if(zip != null) zip.close();
+            } catch(Exception ex) { }
         }
     }
 
-    /**
-     * Opens a resource pack folder, skips to the manual folder, then tries to dissect that
-     */
+    /** Opens a resource pack folder, skips to the manual folder, then tries to dissect that */
     public static void dissectFolder(File folder) {
         File manualFolder = new File(folder, "/assets/manual");
-        if (manualFolder.exists() && manualFolder.isDirectory()) dissectManualFolder(manualFolder);
+        if(manualFolder.exists() && manualFolder.isDirectory()) dissectManualFolder(manualFolder);
     }
 
-    /**
-     * Anal bleeding
-     */
+    /** Anal bleeding */
     public static void dissectManualFolder(File folder) {
 
         File[] files = folder.listFiles();
-        for (File file : files) {
+        for(File file : files) {
             String name = file.getName();
-            if (file.isFile() && name.endsWith(".json")) {
+            if(file.isFile() && name.endsWith(".json")) {
                 try {
                     FileReader reader = new FileReader(file);
                     JsonObject obj = (JsonObject) parser.parse(reader);
                     registerJson(name, obj);
                     logFoundManual(name);
-                } catch (Exception ex) {
+                } catch(Exception ex) {
                     MainRegistry.logger.info("[QMAW] Error reading manual " + name + ": " + ex);
                 }
             }
         }
     }
 
-    /**
-     * Extracts all the info from a json file's main object to add a QMAW to the system. Very barebones, only handles name, icon and the localized text.
-     */
+    /** Extracts all the info from a json file's main object to add a QMAW to the system. Very barebones, only handles name, icon and the localized text. */
     public static void registerJson(String file, JsonObject json) {
 
         String name = json.get("name").getAsString();
         QuickManualAndWiki qmaw = new QuickManualAndWiki(name);
 
-        if (json.has("icon")) {
+        if(json.has("icon")) {
             qmaw.setIcon(SerializableRecipe.readItemStack(json.get("icon").getAsJsonArray()));
         }
 
         JsonObject title = json.get("title").getAsJsonObject();
-        for (Entry<String, JsonElement> part : title.entrySet()) {
+        for(Entry<String, JsonElement> part : title.entrySet()) {
             qmaw.addTitle(part.getKey(), part.getValue().getAsString());
         }
 
         JsonObject content = json.get("content").getAsJsonObject();
-        for (Entry<String, JsonElement> part : content.entrySet()) {
+        for(Entry<String, JsonElement> part : content.entrySet()) {
             qmaw.addLang(part.getKey(), part.getValue().getAsString());
         }
 
         JsonArray triggers = json.get("trigger").getAsJsonArray();
 
-        for (JsonElement element : triggers) {
+        for(JsonElement element : triggers) {
             ItemStack trigger = SerializableRecipe.readItemStack(element.getAsJsonArray());
             // items get renamed and removed all the time, so we add some more debug goodness for those cases
-            if (trigger == null || trigger.getItem() == ModItems.nothing) {
+            if(trigger == null || trigger.getItem() == ModItems.nothing) {
                 MainRegistry.logger.info("[QMAW] Manual " + file + " references nonexistant trigger " + element);
             } else {
                 QMAWLoader.triggers.put(new ComparableStack(trigger).makeSingular(), qmaw);
             }
         }
 
-        if (!qmaw.contents.isEmpty()) {
+        if(!qmaw.contents.isEmpty()) {
             QMAWLoader.qmaw.put(name, qmaw);
         }
-    }
-
-    @Override
-    public void onResourceManagerReload(IResourceManager resourceManager, Predicate<IResourceType> resourcePredicate) {
-        long timestamp = System.currentTimeMillis();
-        MainRegistry.logger.info("[QMAW] Reloading manual...");
-        init();
-        MainRegistry.logger.info("[QMAW] Loaded " + qmaw.size() + " manual entries! (" + (System.currentTimeMillis() - timestamp) + "ms)");
     }
 }
