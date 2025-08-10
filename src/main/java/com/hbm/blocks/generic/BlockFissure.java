d5db4108aafaa70e352e99d60feaf587f33afb1b
mlbv <51232730+mlbv@users.noreply.github.com>
2025-08-10 11:30:38 +0800

    fix: oversight in renderLayer

--- src/main/java/com/hbm/blocks/generic/BlockFissure.java
+++ src/main/java/com/hbm/blocks/generic/BlockFissure.java
@@ -15,10 +15,13 @@ import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.EnumBlockRenderType;
 import net.minecraft.util.ITickable;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 import org.jetbrains.annotations.NotNull;
 
 public class BlockFissure extends BlockContainer {
@@ -42,6 +45,12 @@ public class BlockFissure extends BlockContainer {
     @Override
     public EnumBlockRenderType getRenderType(IBlockState state) {
         return EnumBlockRenderType.MODEL;
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public @NotNull BlockRenderLayer getRenderLayer() {
+        return BlockRenderLayer.TRANSLUCENT;
     }
 
     @Override
