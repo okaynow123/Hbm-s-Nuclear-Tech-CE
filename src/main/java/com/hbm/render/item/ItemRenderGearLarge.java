8b6a87062209dc9c2da7a00ad94f466a0a9a130b
TheSlize <r.region-4@yandex.ru>
2025-08-17 15:49:08 +0300

    fix: issue #412

--- src/main/java/com/hbm/render/item/ItemRenderGearLarge.java
+++ src/main/java/com/hbm/render/item/ItemRenderGearLarge.java
@@ -4,17 +4,17 @@ import com.hbm.interfaces.AutoRegister;
 import com.hbm.main.ResourceManager;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.item.ItemStack;
+import org.lwjgl.opengl.GL11;
 
 import static com.hbm.render.NTMRenderHelper.bindTexture;
 @AutoRegister(item = "gear_large")
 public class ItemRenderGearLarge extends ItemRenderBase {
   @Override
   public void renderInventory() {
-    GlStateManager.translate(3, -5, 0);
+    GL11.glTranslated(0, -7, 0);
     GlStateManager.scale(6, 6, 6);
-    GlStateManager.rotate(-45, 0, 1, 0);
-    GlStateManager.rotate(30, 1, 0, 0);
-    GlStateManager.rotate(90, 1, 0, 0);
+    GL11.glRotated(-45, 0, 1, 0);
+    GL11.glRotated(30, 1, 0, 0);
     GlStateManager.translate(0, 1.375, 0);
     GlStateManager.rotate(System.currentTimeMillis() % 3600 * 0.1F, 0, 0, 1);
     GlStateManager.translate(0, -1.375, 0);
