26defa0cc4ee9d3493ab1c958e399ef27aad0d96
Vladimir <68227685+NoPressF@users.noreply.github.com>
2025-09-06 00:56:37 +0200

    fix: instantly crash server on a startup

--- src/main/java/com/hbm/tileentity/deco/TileEntityBarrier.java
+++ src/main/java/com/hbm/tileentity/deco/TileEntityBarrier.java
@@ -2,11 +2,17 @@ package com.hbm.tileentity.deco;
 
 import com.hbm.interfaces.AutoRegister;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 @AutoRegister
 public class TileEntityBarrier extends TileEntity {
+
+  @Override
+  public AxisAlignedBB getRenderBoundingBox() {
+    return TileEntity.INFINITE_EXTENT_AABB;
+  }
 
   @Override
   @SideOnly(Side.CLIENT)
